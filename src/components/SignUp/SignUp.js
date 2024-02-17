361a5e7d5e1128ee30c2480c4cf47f65ad93211c
Authoi234 <jawadkhan.authoi@gmail.com>
2024-02-17 12:56:26 +0600

    firebase config is added

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const SignUp = () => {
+    const SignUp = () => {
+        
+    }
     return (
-        <div>
-            
+        <div className='mt-3'>
+            <h1 className='break '>Welcome To </h1>
+            <Form className='container'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control required type="text" placeholder="Please Enter Your Name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control required type="email" placeholder="Please Enter Your Email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control required type="password" placeholder="Please Enter Your Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check required type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 };
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZMqzkMjqRHntccoljlVPqwOr04Pmnu1Q",
+  authDomain: "burj-al-arab-b5f96.firebaseapp.com",
+  projectId: "burj-al-arab-b5f96",
+  storageBucket: "burj-al-arab-b5f96.appspot.com",
+  messagingSenderId: "779237103481",
+  appId: "1:779237103481:web:39282a3e731fd5b40141a8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
